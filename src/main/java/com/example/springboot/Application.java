2db0746d676e9bc0ba6798a56b0f3c2b1414b61f
vijaynathari <vijaynathari@gmail.com>
2021-11-12 13:13:57 +0800

    commit

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
