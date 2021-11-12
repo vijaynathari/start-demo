0cd759bb08644c5920a02a99542125929e0f3a0f
vijaynathari <vijaynathari@gmail.com>
2021-11-12 17:35:51 +0800

    commit

--- deliver.sh
+++ deliver.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -x
-brew services restart tomcat
+chmod +x /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh
+. /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh stop
+. /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh stop
