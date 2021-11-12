8333f86ea5e3ebf511a5e55eae65aef6d4b1a72e
vijaynathari <vijaynathari@gmail.com>
2021-11-12 17:54:01 +0800

    commit

--- deliver.sh
+++ deliver.sh
@@ -4,5 +4,4 @@ export CATALINA_HOME=/usr/local/Cellar/tomcat/10.0.12/libexec/bin
 chmod a+x /usr/local/Cellar/tomcat/10.0.12/libexec/bin/setclasspath.sh
 . /usr/local/Cellar/tomcat/10.0.12/libexec/bin/setclasspath.sh
 chmod a+x /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh
-. /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh stop
-. /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina.sh stop
+. /usr/local/Cellar/tomcat/10.0.12/libexec/bin/catalina restart
