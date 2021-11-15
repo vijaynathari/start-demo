8e0c1b226efec5827d8c06f4b9694cbf2ccc6209
vijaynathari <vijaynathari@gmail.com>
2021-11-15 14:15:57 +0800

    coomit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,30 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
-
 	@Test
 	void contextLoads() {
-        App.main(null);
-        try {
-            assertEquals("Greetings from Spring Boot!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Greetings from Spring Boot!\"");
-        }
 	}
 
 }
